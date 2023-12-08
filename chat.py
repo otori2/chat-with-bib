ee5ced641439b8f275c6053b7287e51c8d40f502
tanaka <tanaka@example.com>
2023-12-09 00:33:59 +0900

    Refactored code using LangChain Expression Language (LCEL)

--- chat.py
+++ chat.py
@@ -2,14 +2,14 @@ import os
 import re
 import sys
 from pathlib import Path
+from typing import AsyncIterator
 
 import chainlit as cl
-from chainlit.context import context
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.vectorstores import FAISS
-from openai import AsyncOpenAI
 
 load_dotenv()
 
@@ -17,6 +17,8 @@ api_key = os.getenv("OPENAI_API_KEY", default="sk-")
 index_dir = os.getenv("FAISS_DIR", default="faiss_index")
 model_embedding = os.getenv("EMBEDDING_MODEL", default="text-embedding-ada-002")
 model_chat = os.getenv("CHAT_MODEL", default="gpt-3.5-turbo")
+
+stopwords = ["参考文献"]
 
 # Load FAISS index
 if not Path(index_dir).exists():
@@ -36,11 +38,11 @@ Sources:
 Question:
 {query}
 """
-prompt = PromptTemplate(template=prompt_template, input_variables=["context", "query"])
-
-client = AsyncOpenAI(
-    api_key=api_key,
+prompt_txt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "query"]
 )
+
+model = ChatOpenAI(streaming=True, api_key=api_key)
 
 
 def revise_reference_format(text):
@@ -50,7 +52,53 @@ def revise_reference_format(text):
         text = text.replace(f"。{match}", f"{match}。")
         text = text.replace(f"，{match}", f"{match}，")
         text = text.replace(f"．{match}", f"{match}．")
+    # text = text.replace("$$", "$")
+    text = re.sub(r"\\tag\{\d+(\.\d+)?\}", "", text)
     return text
+
+
+def ref_formatter(text: str, ref_numbers: dict[str, int]) -> str:
+    text = text.replace("、", "，").replace("。", "．")
+    for k in ref_numbers.keys():
+        target = f"[{k}]"
+        if target in text:
+            if ref_numbers[k] == 0:
+                new_value = max(ref_numbers.values()) + 1
+                ref_numbers[k] = new_value
+            text = revise_reference_format(
+                text.replace(target, f"[{str(ref_numbers[k])}]")
+            )
+
+    return text
+
+
+def postprocess(text: str) -> str:
+    text = revise_reference_format(text)
+    for stopword in stopwords:
+        if stopword in text:
+            text = text[: text.index(stopword)].strip()
+    return text
+
+
+async def format_reference(input: AsyncIterator[str]) -> AsyncIterator[str]:
+    ref_numbers = cl.user_session.get("ref_numbers")
+    buffer = ""
+    async for chunk in input:
+        buffer += chunk.content
+        n_bra = sum(1 for c in buffer if c == "[")
+        n_ket = sum(1 for c in buffer if c == "]")
+        if n_bra == n_ket:
+            yield ref_formatter(buffer, ref_numbers)
+            buffer = ""
+        else:
+            cnks = buffer.split("[")
+            cnks = cnks[:1] + ["[" + c for c in cnks[1:]]
+            txt = "".join(cnks[:-1])
+            if txt != "":
+                yield ref_formatter(txt, ref_numbers)
+            buffer = cnks[-1]
+
+    yield buffer
 
 
 @cl.on_chat_start
@@ -59,14 +107,14 @@ def start_chat():
         # Reference: https://github.com/Azure-Samples/azure-search-openai-demo/blob/main/app/backend/approaches/retrievethenread.py
         "message_history",
         [
-            {
-                "role": "system",
-                "content": """\
+            (
+                "system",
+                """\
 You are an intelligent assistant. Use 'you' to refer to the individual asking the questions even if they ask with 'I'. Answer the following question using only the data provided in the sources below. For tabular information return it as an html table. Do not return markdown format. Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. If you cannot answer using the sources below, say you don't know. Use below example to answer. Please be sure to indicate the source of your information when replying.""",
-            },
-            {
-                "role": "user",
-                "content": """\
+            ),
+            (
+                "user",
+                """\
 Sources:
 [info1.txt]:
 deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
@@ -82,40 +130,14 @@ In-network institutions include Overlake, Swedish and others in the region
 
 Question:
 What is the deductible for the employee plan for a visit to Overlake in Bellevue?""",
-            },
-            {
-                "role": "assistant",
-                "content": """\
+            ),
+            (
+                "assistant",
+                """\
 In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].""",
-            },
+            ),
         ],
     )
-
-
-async def stream_token(msg, token: str, ref_numbers: dict[str, int], is_sequence=False):
-    # Override MessageBase.stream_token
-    token = token.replace("、", "，")
-    token = token.replace("。", "．")
-
-    if is_sequence:
-        msg.content = token
-    else:
-        msg.content += token
-
-    for k in ref_numbers.keys():
-        target = f"[{k}]"
-        if target in msg.content:
-            if ref_numbers[k] == 0:
-                new_value = max(ref_numbers.values()) + 1
-                ref_numbers[k] = new_value
-            token = revise_reference_format(
-                token.replace(target, f"[{str(ref_numbers[k])}]")
-            )
-            msg.content = revise_reference_format(
-                msg.content.replace(target, f"[{str(ref_numbers[k])}]")
-            )
-
-    await context.emitter.send_token(id=msg.id, token=token, is_sequence=is_sequence)
 
 
 @cl.on_message
@@ -130,7 +152,9 @@ async def main(message: cl.Message):
         for doc in docs
     }
     ref_numbers = {key: 0 for key in ref_docs.keys()}
-    content = prompt.format(
+    cl.user_session.set("ref_numbers", ref_numbers)
+
+    content = prompt_txt.format(
         context="\n\n".join(
             [
                 f'[{doc.metadata["ID"]}-{doc.metadata["page"]}]:\n{doc.page_content}'
@@ -141,22 +165,21 @@ async def main(message: cl.Message):
     )
 
     message_history = cl.user_session.get("message_history")
-    message_history.append({"role": "user", "content": content})
+    message_history.append(("user", content))
+
+    messages = [
+        (m1, m2.replace("{", "{{").replace("}", "}}")) for m1, m2 in message_history
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    chain = prompt | model | format_reference
 
     msg = cl.Message(content="")
     await msg.send()
 
-    stream = await client.chat.completions.create(
-        messages=message_history,
-        stream=True,
-        model=model_chat,
-        temperature=0.0,
-    )
-
-    async for part in stream:
-        if token := part.choices[0].delta.content or "":
-            await stream_token(msg, token, ref_numbers)
-            # await msg.stream_token(token)
+    async for chunk in chain.astream(message.content):
+        await msg.stream_token(chunk)
+    msg.content = postprocess(msg.content)
 
     ref_numbers = {k: v for k, v in ref_numbers.items() if v > 0}
     if len(ref_numbers) > 0:
@@ -176,5 +199,5 @@ async def main(message: cl.Message):
 
         msg.elements = text_elements
 
-    message_history.append({"role": "assistant", "content": msg.content})
+    message_history.append(("assistant", msg.content))
     await msg.update()
